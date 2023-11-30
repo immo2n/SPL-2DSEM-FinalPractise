81bdb46a99114f8799fe0a6f4eb87f503d8f9f59
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-11-30 13:43:14 +0600

    Q4

---
+++ 10-Students.c
@@ -0,0 +1,33 @@
+//4. Write a Code in C to Store information of 10 students using structures
+
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    char id[15];
+    int age;
+    char phone[11];
+};
+
+int main(){
+
+    struct Student student[10];
+
+    for(int x = 0; x < 10; x++){
+        printf("Enter information for student: %d\n", x+1);
+        scanf("%s", &student[x].name);
+        scanf("%s", &student[x].id);
+        scanf("%d", &student[x].age);
+        scanf("%s", &student[x].phone);
+    }
+
+    for(int x = 0; x < 10; x++){
+        printf("Name: %s\n", student[x].name);
+        printf("ID: %s\n", student[x].id);
+        printf("Age: %d\n", student[x].age);
+        printf("Phone: %s\n", student[x].phone);
+    }
+
+    return 0;
+}
