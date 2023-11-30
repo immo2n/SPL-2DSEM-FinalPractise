9c762a101cd6ffdffaaf0d86453a2b5665883efd
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-11-30 23:53:39 +0600

    Q1

---
+++ 2D-Array/SumOfRows.c
@@ -0,0 +1,24 @@
+/*1. Write a program in C to show the sum of rows.
+|2 2 3|
+|4 5 6|
+|7 8 9|
+*/
+#include <stdio.h>
+int main()
+{
+    int matrix[3][3] = {
+        {2, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for(int i = 0; i < 3; i++){
+        int sum = 0;
+        for(int j = 0; j < 3; j++){
+            sum += matrix[i][j];
+        }
+        printf("Sum of row number %d is: %d\n", i+1, sum);
+    }
+
+    return 0;
+}
