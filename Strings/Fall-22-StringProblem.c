c4f259c0db8b4d0ccc1dae5433f1ea67f823b27f
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-12-01 19:04:14 +0600

    QF

---
+++ Strings/Fall-22-StringProblem.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[50];
+    char str2[50];
+    char str3[50];
+
+    strcpy(str1, "Beautiful ");
+    strcpy(str2, "Bangladesh");
+
+    int len;
+
+    len = strlen(str1);
+    printf("strlen (str1) : %d\n", len);
+
+    strcpy(str3, str2);
+    printf("strcpy( str 3, str2): %s\n", str3);
+
+    strncpy(str3, str2, 5);
+    printf("strncpy( str1, str2,5): %s\n", str3);
+
+    strcat(str1, str2);
+    printf("strcat( str1, str2): %s\n", str1);
+
+    strncat(str1, str2, 3);
+    printf("strncat( str1, str2,3): %s\n", str1);
+
+    return 0;
+}
