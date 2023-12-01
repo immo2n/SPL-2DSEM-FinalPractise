428bf66e5a28b415b4205bd8907432ff60af5149
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-12-01 23:26:44 +0600

    Q5

---
+++ Strings/CharacterFrequency.c
@@ -0,0 +1,35 @@
+//5. Write a program in C to find the frequency of characters.
+#include <stdio.h>
+#include <string.h>
+int alreadyDone(char c, char done[], int doneSize){
+    for(int i = 0; i < doneSize; i++){
+        if(done[i] == c){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(){
+    char string[200];
+    printf("Enter the string:\n");
+    gets(string);
+
+    char done[100];
+    int found = 0;
+
+    for(int i = 0; i < strlen(string); i++){
+        if(alreadyDone(string[i], done, strlen(done))){
+            continue;
+        }
+        int count = 0;
+        for(int j = 0; j < strlen(string); j++){
+            if(string[j] == string[i]){
+                count++;
+            }
+        }
+        done[found] = string[i];
+        found++;
+        printf("%c: %d\n", string[i], count);
+    }
+
+}
